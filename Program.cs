681ba9667e032d03d0cc687273156c3fa8e4a670
kolya5544 <05-kolya@bk.ru>
2020-02-15 10:22:40 +0300

    Stuff working fine! This should help beginners to make their first  HTTP server.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -11,7 +12,7 @@ namespace HTTPServer
         {
             if (args.Length > 0) {
                 var port = int.Parse(args[0]);
-                Console.WriteLine("=[ Starting HTTP Webserver at "+args[0]+" port!");
+                Console.WriteLine("=[ Starting HTTP Webserver at "+args[0]+" port! ]=");
 
                 TcpListener tcp = new TcpListener(IPAddress.Any, port);
                 tcp.Start();
@@ -25,8 +26,67 @@ namespace HTTPServer
                         var ns = client.GetStream();
                         ns.ReadTimeout = 2000;
                         ns.WriteTimeout = 2000;
+                        var sr = new StreamReader(ns);
+                        var sw = new StreamWriter(ns);
+                        sw.AutoFlush = true;
+
+                        Console.WriteLine("-> New client connected!");
+
+                        bool DataAccepted = false;
+
+                        var UserThread = new Thread(() => {
+                            Thread.CurrentThread.IsBackground = true;
+
+                            //RECEIVER HANDLING
+                            string Builder = "";
+                            while (!DataAccepted && client.Connected)
+                            {
+                                try
+                                {
+                                    string answ = sr.ReadLine();
+                                    
+                                    if (answ.Length < 1)
+                                    {
+                                        DataAccepted = true;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Answ - " + answ);
+                                        Builder += answ + "\r\n";
+                                    }
+                                } catch
+                                {
+
+                                }
+                            }
+
+                            //REQUEST HANDLING
+                            string[] request = Builder.Split("\r\n");
+                            if (request[0].StartsWith("GET"))
+                            {
+                                string filename = request[0].Split(' ')[1];
+
+                                //Default 200 response
+                                sw.Write("HTTP/1.1 200 OK\r\n");
+                                sw.Write("Server: IKTeam\r\n");
+                                sw.Write("Content-Type: text/html; charset=utf-8\r\n");
+                                sw.Write("Connection: close\r\n");
+
+                                //Payload
+                                sw.Write("\r\n");
+                                sw.Write("<html><head><h1>Hello! You tried to request - "+filename+"</h1></head></html>\r\n");
+                                client.Close();
+                            }
+                            
+                        });
+                        UserThread.Start();
                     }
                 });
+                ListenThread.Start();
+                while (true)
+                {
+                    Thread.Sleep(2000);
+                }
             }
         }
     }
