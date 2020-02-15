cd9f080f3784fca9c41368293c8677d606a29a20
kolya5544 <05-kolya@bk.ru>
2020-02-15 10:00:51 +0300

    Adding listening stuff

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace HTTPServer
 {
@@ -6,7 +9,25 @@ namespace HTTPServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 0) {
+                var port = int.Parse(args[0]);
+                Console.WriteLine("=[ Starting HTTP Webserver at "+args[0]+" port!");
+
+                TcpListener tcp = new TcpListener(IPAddress.Any, port);
+                tcp.Start();
+                var ListenThread = new Thread(() => {
+                    Thread.CurrentThread.IsBackground = true;
+
+                    while (true)
+                    {
+                        TcpClient client = tcp.AcceptTcpClient();
+
+                        var ns = client.GetStream();
+                        ns.ReadTimeout = 2000;
+                        ns.WriteTimeout = 2000;
+                    }
+                });
+            }
         }
     }
 }
